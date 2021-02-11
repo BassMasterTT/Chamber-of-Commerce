2d726a57f0bb577179ad191d7cde075e7d8441f7
BassMasterTT <tothtibi22@gmail.com>
2021-02-11 20:07:31 +0800

    Home , More Posts, Lorem Ipsum

--- packages/Chamber-of-Commerce-theme/src/theme-files/index.js
+++ packages/Chamber-of-Commerce-theme/src/theme-files/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { connect } from "frontity";
+import Link from "./link";
 
-const Root = () => {
+const Root = ({ state }) => {
   return (
     <>
-      <h1>Frontify Workshop 123</h1>
+      <h1>Frontity Workshop</h1>
+      <p>Current URL: {state.router.link}</p>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/page/2">More Posts</Link>
+        <Link href="lorem-ipsum">Lorem Ipsum</Link>
+      </nav>
     </>
   );
 };
 
-export default Root;
+export default connect(Root);
