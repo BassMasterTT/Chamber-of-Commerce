78f6c0d7079d5fc6c378adceb5e06833eda2d055
BassMasterTT <tothtibi22@gmail.com>
2021-02-12 15:50:51 +0800

    styling the header and header content

---
+++ build/static/chamber-of-commerce.fe5440a6545af8198f08.hot-update.js
@@ -0,0 +1,26 @@
+webpackHotUpdate("chamber-of-commerce",{
+
+/***/ "./packages/Chamber-of-Commerce-theme/src/theme-files/index.js":
+/*!*********************************************************************!*\
+  !*** ./packages/Chamber-of-Commerce-theme/src/theme-files/index.js ***!
+  \*********************************************************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: D:\\\\Chamber_Of_Commerce_Frontify\\\\Chamber-of-Commerce\\\\packages\\\\Chamber-of-Commerce-theme\\\\src\\\\theme-files\\\\index.js: Unexpected token (9:0)\\n\\n\\u001b[0m \\u001b[90m  7 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[33mStyled\\u001b[39m \\u001b[36mfrom\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m  8 |\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m  9 |\\u001b[39m \\u001b[36mconst\\u001b[39m \\u001b[33mRoot\\u001b[39m \\u001b[33m=\\u001b[39m ({ state }) \\u001b[33m=>\\u001b[39m {\\u001b[0m\\n\\u001b[0m \\u001b[90m    |\\u001b[39m \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 10 |\\u001b[39m   \\u001b[36mconst\\u001b[39m data \\u001b[33m=\\u001b[39m state\\u001b[33m.\\u001b[39msource\\u001b[33m.\\u001b[39m\\u001b[36mget\\u001b[39m(state\\u001b[33m.\\u001b[39mrouter\\u001b[33m.\\u001b[39mlink)\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 11 |\\u001b[39m   \\u001b[36mreturn\\u001b[39m (\\u001b[0m\\n\\u001b[0m \\u001b[90m 12 |\\u001b[39m     \\u001b[33m<\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n    at Object._raise (D:\\\\Chamber_Of_Commerce_Frontify\\\\Chamber-of-Commerce\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\error.js:97:45)\\n    at Object.raiseWithData (D:\\\\Chamber_Of_Commerce_Frontify\\\\Chamber-of-Commerce\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\error.js:92:17)\\n    at Object.raise (D:\\\\Chamber_Of_Commerce_Frontify\\\\Chamber-of-Commerce\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\error.js:41:17)\\n    at Object.unexpected (D:\\\\Chamber_Of_Commerce_Frontify\\\\Chamber-of-Commerce\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\util.js:140:16)\\n    at Object.parseImportSource (D:\\\\Chamber_Of_Commerce_Frontify\\\\Chamber-of-Commerce\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\statement.js:2158:38)\\n    at Object.parseImport (D:\\\\Chamber_Of_Commerce_Frontify\\\\Chamber-of-Commerce\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\statement.js:2137:24)\\n    at Object.parseStatementContent (D:\\\\Chamber_Of_Commerce_Frontify\\\\Chamber-of-Commerce\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\statement.js:247:25)\\n    at Object.parseStatement (D:\\\\Chamber_Of_Commerce_Frontify\\\\Chamber-of-Commerce\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\statement.js:158:17)\\n    at Object.parseBlockOrModuleBlockBody (D:\\\\Chamber_Of_Commerce_Frontify\\\\Chamber-of-Commerce\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\statement.js:875:25)\\n    at Object.parseBlockBody (D:\\\\Chamber_Of_Commerce_Frontify\\\\Chamber-of-Commerce\\\\node_modules\\\\@babel\\\\parser\\\\src\\\\parser\\\\statement.js:851:10)\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3BhY2thZ2VzL0NoYW1iZXItb2YtQ29tbWVyY2UtdGhlbWUvc3JjL3RoZW1lLWZpbGVzL2luZGV4LmpzLmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./packages/Chamber-of-Commerce-theme/src/theme-files/index.js\n");
+
+/***/ }),
+
+/***/ "./packages/Chamber-of-Commerce-theme/src/theme-files/link.js":
+false,
+
+/***/ "./packages/Chamber-of-Commerce-theme/src/theme-files/list.js":
+false,
+
+/***/ "./packages/Chamber-of-Commerce-theme/src/theme-files/page.js":
+false,
+
+/***/ "./packages/Chamber-of-Commerce-theme/src/theme-files/post.js":
+false
+
+})
--- packages/Chamber-of-Commerce-theme/src/theme-files/index.js
+++ packages/Chamber-of-Commerce-theme/src/theme-files/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from "frontity";
+import { connect, Global, css, styled } from "frontity";
 import Link from "./link";
 import List from "./list";
 import Post from "./post";
@@ -9,14 +9,30 @@ const Root = ({ state }) => {
   const data = state.source.get(state.router.link);
   return (
     <>
-      <h1>Frontity Workshop</h1>
-      <p>Current URL: {state.router.link}</p>
-      <nav>
-        <Link href="/">Home</Link>
-        <Link href="/page/2">More Posts</Link>
-        <Link href="/lorem-ipsum">Lorem Ipsum</Link>
-      </nav>
-      <hr />
+      <Global
+        styles={css`
+          * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+          }
+          html {
+            font-family: sans-serif;
+          }
+        `}
+      />
+      <Header>
+        <HeaderContent>
+          <h1>Frontity Workshop</h1>
+          <p>Current URL: {state.router.link}</p>
+          <nav>
+            <Link href="/">Home</Link>
+            <Link href="/page/2">More Posts</Link>
+            <Link href="/lorem-ipsum">Lorem Ipsum</Link>
+          </nav>
+        </HeaderContent>
+      </Header>
+
       <main>
         {data.isArchive && <List />}
         {data.isPost && <Post />}
@@ -27,3 +43,15 @@ const Root = ({ state }) => {
 };
 
 export default connect(Root);
+
+const Header = styled.header`
+  background-color: #eee;
+  border-width: 0 0 8px 0;
+  border-style: solid;
+  border-color: maroon;
+`;
+const HeaderContent = styled.div`
+  max-width: 800px;
+  padding: 2em 1em;
+  margin: auto;
+`;
